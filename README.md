fd8330c22a8076828884b6f91e09b8494c2fa6b7
Zen <muhzaini30@gmail.com>
2022-10-25 16:02:41 +0800

    simpan dulu

--- README.md
+++ README.md
@@ -36,6 +36,7 @@ lpm c
 
 ## TODO
 
+- [ ] modify rockspec file (using: luarocks --tree lua_modules/ list)
 - [ ] lpm version
 - [ ] lpm run
 - [ ] lpm test
--- lpm.lua
+++ lpm.lua
@@ -3,24 +3,25 @@
 if arg[1] == "i" then
   -- install dari rockspec dulu
   local popen_rockspec = io.popen("ls *.rockspec")
-  local rockspec = popen_rockspec:read("*a")
-    rockspec = rockspec:gsub("^%s*(.-)%s*$", "%1")
+    local rockspec = popen_rockspec:read("*a")
+    rockspec = rockspec:gsub("^%s*(.-)%s*$", "%1") -- nama file rockspec-nya
     os.execute("luarocks install --tree lua_modules \"" .. rockspec .. "\" --only-deps")
-  popen_rockspec:close()
 
-  if arg[2] then -- contoh: lpm i a b c
-    for n, x in ipairs(arg) do
-      if n > 1 then
-        os.execute("luarocks install " .. x .. " --tree lua_modules")
+    if arg[2] then -- contoh: lpm i a b c
+      for n, x in ipairs(arg) do
+        if n > 1 then
+          -- masukkan ke rockspec
+          os.execute("luarocks install " .. x .. " --tree lua_modules")
+        end
       end
     end
-  end
 
+  popen_rockspec:close()
 end
 
 if arg[1] == "init" then -- sudah
   local popen_folder = io.popen("basename $(pwd)")
-  local folder = popen_folder:read("*a")
+    local folder = popen_folder:read("*a")
     folder = folder:gsub("^%s*(.-)%s*$", "%1")
     local file = io.open(folder .. "-0.0-1.rockspec", "w")
       file:write([[
